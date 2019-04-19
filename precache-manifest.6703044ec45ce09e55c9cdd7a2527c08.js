c37e8ec9aa25d3c80e45767b7e8299597d64580c
Bojan Krstic <krstic.bojan80@gmail.com>
2019-04-19 11:21:06 +0200

    Updates

---
+++ precache-manifest.6703044ec45ce09e55c9cdd7a2527c08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f3041040872d1a52efc",
+    "url": "/best-recipes/static/js/runtime~main.b5b93d29.js"
+  },
+  {
+    "revision": "fd184cd8b6d60e618aff",
+    "url": "/best-recipes/static/js/main.2ccb56c2.chunk.js"
+  },
+  {
+    "revision": "cbd3ffddf8a95d7e39e5",
+    "url": "/best-recipes/static/js/2.8c3ad494.chunk.js"
+  },
+  {
+    "revision": "fd184cd8b6d60e618aff",
+    "url": "/best-recipes/static/css/main.d4b7ab96.chunk.css"
+  },
+  {
+    "revision": "62eb5459e2751b9f5a60ff8520139914",
+    "url": "/best-recipes/index.html"
+  }
+];
